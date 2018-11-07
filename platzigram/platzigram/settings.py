e4be5d61bca72b9f7e91b79539ab3b459c887d65
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-07 12:49:21 -0600

    Agregando app de usuarios para los perfiles

--- platzigram/platzigram/settings.py
+++ platzigram/platzigram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Apps
     'post',
+    'users',
 ]
 
 MIDDLEWARE = [
---
+++ platzigram/users/admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import Profile
+
+# Register your models here.
+# admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """ ProfileAdmin """
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name', 'phone_number')
+    list_filter = ('created', 'modified', 'user__is_active')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                ('user', 'picture'),)
+        }),
+        ('ExtraInfo', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography',)
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),)
+        }),
+    )
+
+    readonly_fields = ('created', 'modified')
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile 
+    can_delete = False 
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
