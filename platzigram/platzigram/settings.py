63685010e01305dcb41137addc434bbf7c7b5637
Andrés Aguilar <andresyoshimar@gmail.com>
2018-11-07 13:33:48 -0600

    Configurando templates y archivos estaticos

--- platzigram/platzigram/settings.py
+++ platzigram/platzigram/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'platzigram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls.static import static
 
 from post.views import list_posts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
